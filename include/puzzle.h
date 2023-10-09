193a3af89dec3229859c351808ba8db50da53791
zbuster05 <zakary.saya@gmail.com>
2023-10-09 16:34:17 -0400

    reorg

---
+++ include/puzzle.h
@@ -0,0 +1,12 @@
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
+#include "ppm_io.h" // for Image data type
+
+typedef struct {
+  // ...fields go here...
+} Puzzle;
+
+// TODO: function declarations
+
+#endif // PUZZLE_H
---
+++ src/puzzle_funcs.c
@@ -0,0 +1,8 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include "puzzle.h"
+
+// TODO: implement functions
