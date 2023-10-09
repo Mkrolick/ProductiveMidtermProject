f23a9223a2b0238856aa9bc17effc24a1eaa85ca
zbuster05 <zakary.saya@gmail.com>
2023-10-09 17:04:50 -0400

    updated file

--- include/puzzle.h
+++ include/puzzle.h
@@ -7,6 +7,9 @@ typedef struct {
   // ...fields go here...
 } Puzzle;
 
-// TODO: function declarations
+Puzzle *puzzle_create(int size);
+void puzzle_destroy(Puzzle *p);
+void puzzle_set_tile(Puzzle *p, int col, int row, int value);
+int puzzle_get_tile(const Puzzle *p, int col, int row);
 
 #endif // PUZZLE_H
