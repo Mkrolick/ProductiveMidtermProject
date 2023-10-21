fe09e85954d54945f60d453a4dfe4d18c7a3271c
Malcolm Krolick <malcolm.krolick@gmail.com>
2023-10-21 19:03:33 -0400

    Updated puzzle_funcs.c
    Added puzzle destroy function and edited the puzzle create function.

--- src/puzzle_funcs.c
+++ src/puzzle_funcs.c
@@ -12,8 +12,13 @@ Puzzle* puzzle_create(int size) {
   // need to check if pointers worked or failed
   Puzzle* new_puzzle = malloc(sizeof(Puzzle));
 
-  //creating 1D array of tile poninters
-  new_puzzle->tiles = malloc(sizeof(Tile*) * size);
+  //creating 2D array of tile poninters
+  Tile** tiles = malloc(sizeof(Tile*) * size);
+  for (int i = 0; i < size; i++) {
+    tiles[i] = malloc(sizeof(Tile) * size);
+  }
+
+  new_puzzle->tiles = tiles;
 
   // creating 2D array of tile positions
   int** positions = malloc(sizeof(int*) * size);
@@ -29,7 +34,27 @@ Puzzle* puzzle_create(int size) {
 
 void puzzle_destroy(Puzzle *p) {
 
-    p->positions
+    //freeing positions
+    for (int i = 0; i < (p->size); i++) {
+        free(p->positions[i]);
+    }
+    free(p->positions);
 
+    // freeing tiles
+    for (int i = 0; i < (p->size); i++) {
+        // Note: how to check if valid tile?
 
+        Tile* temp_tile = p->tiles[i];
+        // free temp_tile image ptr
+        free(temp_tile->imageBlock);
+
+        
+        free(p->tiles[i]);
+    }
+    free(p->tiles);
+
+    // set the freed pointers to NULL
+    p->positions = NULL;
+    p->tiles = NULL;
+    
 }
