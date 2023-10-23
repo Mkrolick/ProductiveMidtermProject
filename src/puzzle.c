0aabda6d93d679b894d4e0fb8bfa479f040a8d77
Malcolm Krolick <malcolm.krolick@gmail.com>
2023-10-23 15:21:13 -0400

    Updated Puzzle.c
    - Added S command first draft

--- src/puzzle.c
+++ src/puzzle.c
@@ -244,7 +244,6 @@ int handle_W_command(FILE* in, Image *im, Puzzle *p) {
 
   for (int row = 0; row < p->size; row++) {
     for (int col = 0; col < (p->size); col++) {
-      //aaaaaaaaAAAAaaaaaaAAA - incoherent screaming
     }
   }
 
@@ -326,12 +325,13 @@ int handle_S_command(FILE* fp, Puzzle *p) {
       }
 
       break;
+    
     case 'l':
 
       // NEed to UPDATE YAY
 
       for (int row = 0; row < (p->size); row++) {
-        for (int col = 1; col < (p->size); col++) {
+        for (int col = 0; col < ((p->size) -1); col++) {
           if (puzzle_get_tile(p, col, row) == 0) {
             found_col = col;
             found_row = col;
@@ -347,22 +347,23 @@ int handle_S_command(FILE* fp, Puzzle *p) {
 
       temp[0] = 0;
 
-      for (int row = 1; row < (p->size); row++) {
-        if (puzzle_get_tile(p, found_col, row) != 0) {
-          temp[row] = puzzle_get_tile(p, found_col, row);
+      for (int col = 1; col < (p->size); col++) {
+        if (puzzle_get_tile(p, col, found_row) != 0) {
+          temp[col] = puzzle_get_tile(p, col, found_row);
         }
       }
       
       for (int row = 0; row < (p->size); row++) {
-        puzzle_set_tile(p, found_col, row, temp[row]);
+        puzzle_set_tile(p, found_col, ((p->size)-1)-row, temp[row]);
       }
       
       break;
+
     case 'r':
 
       // UPdate YAY
       for (int row = 1; row < (p->size); row++) {
-        for (int col = 0; col < ((p->size) - 1); col++) {
+        for (int col = 0; col < (p->size); col++) {
           if (puzzle_get_tile(p, col, row) == 0) {
             found_col = col;
             found_row = col;
@@ -384,10 +385,11 @@ int handle_S_command(FILE* fp, Puzzle *p) {
         }
       }
       
-      for (int row = 0; row < (p->size); row++) {
-        puzzle_set_tile(p, found_col, row, temp[row]);
+      for (int col = 0; col < (p->size); col++) {
+        puzzle_set_tile(p, col, found_row, temp[col]);
       }
 
+    
       
       break;
     default:
