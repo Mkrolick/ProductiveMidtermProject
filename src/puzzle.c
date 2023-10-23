e81f907da5f49d576a1afb8a957a29b9abf657f7
Malcolm Krolick <malcolm.krolick@gmail.com>
2023-10-22 21:33:17 -0400

    Refactored puzzle.c
    Moved around code segments into functions corresponding to their position in the switch statement as declared in the previous header update.

--- src/puzzle.c
+++ src/puzzle.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
   int size;
   int res = fscanf(fp, " %c", command);
 
-  Puzzle* p = NULL;
+  Puzzle** p = NULL;
   Image* img_ptr = NULL;
   
 
@@ -37,98 +37,32 @@ int main(int argc, char **argv) {
     {
     case 'C':
 
-      // Checking if size is read in properly
-      if ((fscanf(fp, " %d", &size) != 1)) {
-        fprintf(stderr, "Invalid input");
+      if (handle_C_command(fp, p) != 0) {
         return 1;
       }
-
-      // Checking if size is out of bounds
-      if (size < 2 || size > 20) {
-        fprintf(stderr, "Invalid puzzle size");
-        return 1;
-      }
-
-
-      // Assume pointer is valid - as no error code
-      p = puzzle_create(size);
-
-
 
       puzzle_created = 1;
       break;
+
     case 'T':
       if (!puzzle_created) {
         fprintf(stderr, "No puzzle");
         return 1;
       }
 
-      int *temp_arr = malloc(sizeof(int) * size * size);
-
-
-      // seting up empty array to compare values from 
-      for (int i = 0; i < size * size; i++) {
-        *(temp_arr + i) = 0;
+      if (andle_T_command(fp, *p) != 0) {
+        return 1;
       }
-
-      int temp;
-
-      for (int row = 0; row < size; row++) {
-        for (int col = 0; col < size; col++) {
-
-            if (fp, fscanf(" %d", &temp) == 1) {
-
-              // calculates the address in memory for the temp_arr where val is present
-              // pointer is not acessed unless temp is valid
-              int* current_pointer = temp_arr + (size*(temp/size)) + temp/size;
-
-              if (temp > 16 || temp < 0) {
-                // is that invalid input or invalid tile value
-                fprintf(stderr, "Invalid tile value");
-                return 1;
-              }
-              else if (*current_pointer == 0) {
-                puzzle_set_tile(p, col, row, temp);
-                *current_pointer += 1;
-              } else {
-                fprintf(stderr, "Invalid tile value");
-                return 1;
-              }
-
-            } else {
-              fprintf(stderr, "Invalid input");
-              return 1;
-              
-            }
-        }
-      }
-
-      free(temp_arr);
-
 
       break;
+
     case 'I':
       // check if this is correct array size
-      char arr[256] = {'\0'};
+      img_ptr = handle_I_command(fp, *p);
 
-      if (fscanf(fp," %s", &arr) != 1) {
-        fprintf(stderr, "Invalid input");
+      if (img_ptr == NULL) {
         return 1;
       }
-
-      // change read to write in the future?
-      FILE* img_file_ptr = fopen(arr, "r");
-      
-      // check if valid file pointer and if valid file header
-      if ((!img_file_ptr) && (ReadNum(img_file_ptr) != -1)){
-        fprintf(stderr, "Could not open image file '%s'", arr);
-        return 1;
-      }
-
-      img_ptr = ReadPPM(img_file_ptr);
-
-      //freeing file pointer after function
-      free(img_file_ptr);
 
       break;
     case 'P':
@@ -196,3 +130,116 @@ int main(int argc, char **argv) {
 }
 
 
+
+
+
+
+int handle_C_command(FILE *in, Puzzle **p) {
+
+  // differences in return are to help in debugging
+  int size = 0;
+
+  // Checking if size is read in properly
+  if ((fscanf(in, " %d", &size) != 1)) {
+    fprintf(stderr, "Invalid input");
+    return 1;
+  }
+
+  // Checking if size is out of bounds
+  if (size < 2 || size > 20) {
+    fprintf(stderr, "Invalid puzzle size");
+    return 2;
+  }
+
+  // Assume pointer is valid - as no error code
+  *p = puzzle_create(size);
+
+  return 0;
+}
+
+
+
+
+
+
+
+int handle_T_command(FILE *in, Puzzle *p) {
+  int size = p->size;
+
+  int *temp_arr = malloc(sizeof(int) * size * size);
+
+
+    // seting up empty array to compare values from 
+    for (int i = 0; i < size * size; i++) {
+      *(temp_arr + i) = 0;
+    }
+
+    int temp;
+
+    for (int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+
+          if (in, fscanf(" %d", &temp) == 1) {
+
+            // calculates the address in memory for the temp_arr where val is present
+            // pointer is not acessed unless temp is valid
+            int* current_pointer = temp_arr + (size*(temp/size)) + temp/size;
+
+            if (temp > 16 || temp < 0) {
+              // is that invalid input or invalid tile value
+              fprintf(stderr, "Invalid tile value");
+              return 1;
+            }
+            else if (*current_pointer == 0) {
+              puzzle_set_tile(p, col, row, temp);
+              *current_pointer += 1;
+            } else {
+              fprintf(stderr, "Invalid tile value");
+              return 2;
+            }
+
+          } else {
+            fprintf(stderr, "Invalid input");
+            return 3;
+            
+          }
+      }
+    }
+
+    free(temp_arr);
+
+    return 0;
+}
+
+
+
+
+
+
+
+// Do I make this return back the image_ptr
+Image* handle_I_command(FILE *in, Puzzle *p) {
+  // check if this is correct array size
+  char arr[256] = {'\0'};
+
+  if (fscanf(in, " %s", &arr) != 1) {
+    fprintf(stderr, "Invalid input");
+    return 1;
+  }
+
+  // change read to write in the future?
+  FILE* img_file_ptr = fopen(arr, "r");
+
+  // check if valid file pointer and if valid file header
+  if ((!img_file_ptr) && (ReadNum(img_file_ptr) != -1)){
+    fprintf(stderr, "Could not open image file '%s'", arr);
+    // can't get to free
+    free(img_file_ptr);
+    return NULL;
+  }
+
+  //freeing file pointer after function
+  free(img_file_ptr);
+
+  return ReadPPM(img_file_ptr);
+}
